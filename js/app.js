966715188619d172a38640511c9d9fc6f9a34ad4
CS04 <jerry.madoque@web.de>
2026-02-14 20:00:29 +0100

    forgot to remove borders

--- js/app.js
+++ js/app.js
@@ -1713,7 +1713,7 @@ poker.init();
  * - AUTO_RELOAD_ON_SW_UPDATE: reload page once after an update
  -------------------------------------------------------------------------------------------------- */
 const USE_SERVICE_WORKER = true;
-const SERVICE_WORKER_VERSION = "2026-02-14-v1957";
+const SERVICE_WORKER_VERSION = "2026-02-14-v1959";
 const AUTO_RELOAD_ON_SW_UPDATE = true;
 
 /* --------------------------------------------------------------------------------------------------
