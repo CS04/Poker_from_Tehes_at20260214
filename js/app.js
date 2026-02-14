1682d5d7cacaafc4efbfea8cf80f10489aae035f
CS04 <jerry.madoque@web.de>
2026-02-14 19:58:06 +0100

    Grid for 10 players without overlap

--- js/app.js
+++ js/app.js
@@ -1713,7 +1713,7 @@ poker.init();
  * - AUTO_RELOAD_ON_SW_UPDATE: reload page once after an update
  -------------------------------------------------------------------------------------------------- */
 const USE_SERVICE_WORKER = true;
-const SERVICE_WORKER_VERSION = "2026-02-14-v1502";
+const SERVICE_WORKER_VERSION = "2026-02-14-v1957";
 const AUTO_RELOAD_ON_SW_UPDATE = true;
 
 /* --------------------------------------------------------------------------------------------------
